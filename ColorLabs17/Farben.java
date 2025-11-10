c3ff990c58b1a641c67e7ace90cd83d34ac4ad25
BeanTheAlien <ben.allen.goldstein@gmail.com>
2025-11-10 08:34:14 -0600

    we are in a world full of magic
    AHHHH

---
+++ ColorLabs17/Farben.java
@@ -0,0 +1,80 @@
+
+/**
+ * Write a description of class Farben here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.awt.*;
+import java.util.*;
+public class Farben
+{
+    public static void main(String[] args) {
+        Picture bug = new Picture("images/caterpillar.jpg");
+        Picture bug1 = new Picture("images/caterpillar.jpg");
+        Picture bug2 = new Picture("images/caterpillar.jpg");
+        Picture bug3 = new Picture("images/caterpillar.jpg");
+        Picture bug4 = new Picture("images/caterpillar.jpg");
+        Picture bug5 = new Picture("images/caterpillar.jpg");
+        Picture bug6 = new Picture("images/caterpillar.jpg");
+        
+        Pixel[] pxs = bug.getPixels();
+        for(Pixel px : pxs) {
+            px.setRed(mult(px.getRed(), 0.5));
+            px.setGreen(mult(px.getGreen(), 1.5));
+            px.setBlue(mult(px.getBlue(), 0.25));
+        }
+        bug.explore();
+        
+        Pixel[] pxs1 = bug1.getPixels();
+        for(Pixel px : pxs1) {
+            px.setRed(neg(px.getRed()));
+            px.setGreen(neg(px.getGreen()));
+            px.setBlue(neg(px.getBlue()));
+        }
+        bug1.explore();
+        
+        Pixel[] pxs2 = bug2.getPixels();
+        for(Pixel px : pxs2) {
+            int avg = (int)(px.getAverage());
+            px.setColor(new Color(avg, avg, avg));
+        }
+        bug2.explore();
+        
+        Pixel[] pxs3 = bug3.getPixels();
+        for(Pixel px : pxs3) {
+            px.setRed(light(px.getRed()));
+            px.setGreen(light(px.getGreen()));
+            px.setBlue(light(px.getBlue()));
+        }
+        bug3.explore();
+        
+        Pixel[] pxs4 = bug4.getPixels();
+        for(Pixel px : pxs4) {
+            px.setRed(dark(px.getRed()));
+            px.setGreen(dark(px.getGreen()));
+            px.setBlue(dark(px.getBlue()));
+        }
+        bug4.explore();
+        
+        Pixel[] pxs5 = bug5.getPixels();
+        for(Pixel px : pxs5) {
+            px.setRed(nextColour(px.getRed(), 50));
+            px.setGreen(nextColour(px.getGreen(), -30));
+            px.setBlue(nextColour(px.getBlue(), 2));
+        }
+        bug5.explore();
+        
+        Pixel[] pxs6 = bug6.getPixels();
+        for(Pixel px : pxs6) {
+            px.setRed(demon(px.getRed()));
+        }
+        bug6.explore();
+    }
+    public static int mult(int val, double f) { return (int)(val * f); }
+    public static int neg(int v) { return 255 - v; }
+    public static int light(int v) { return (int)(v * 3); }
+    public static int dark(int v) { return (int)(v / 3); }
+    public static int nextColour(int v, int b) { return Math.min(255, v + b); }
+    public static int demon(int r) { return Math.min(255, r + (new Random().nextInt(-100, 101))); }
+}
--- ColorLabs17/SheparFaireyLab.java
+++ ColorLabs17/SheparFaireyLab.java
@@ -15,36 +15,63 @@ public class SheparFaireyLab
      * main method, to test the picture
      *  
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        //opens selfie picture
+        /*String fileName = FileChooser.pickAFile();
+        Picture pictObj = new Picture(fileName);
+        pictObj.explore();*/
+        //relative path
+        //Picture apic = new Picture("images\\beach.jpg");
+        //change with selfie picture
+        //Picture me = new Picture("images/beach.jpg");
+        //Picture me1 = new Picture("images/beach.jpg");
+        //Picture me2 = new Picture("images/beach.jpg"); 
         
-         //opens selfie picture 
-          /**/
-         String fileName = FileChooser.pickAFile();
-         Picture pictObj = new Picture(fileName);
-         pictObj.explore();
-         
-         //relative path
-         Picture apic = new Picture("images\\beach.jpg");
-         //change with selfie picture
-         Picture me = new Picture("images/beach.jpg");
-         Picture me1 = new Picture("images/beach.jpg");
-         Picture me2 = new Picture("images/beach.jpg");
-         
-         /**
-          * method 1 change
-          * 
-          */
-         
-         /**
-          * method 2 change
-          * 
-          */
-         
-         /**
-          * custom color palette
-          */
-
-         
-    }//main       
-}//class
+        // Pixel.getColor(): Color
+        // Pixel.getRed(): int
+        // Pixel.getGreen(): int
+        // Pixel.getBlue(): int
+        // Pixel.getAlpha(): int
+        // Pixel.getAverage(): double
+        // Pixel.setColor(newColor: Color): void
+        // Pixel.setRed(int value): void
+        // Pixel.setGreen(int value): void
+        // Pixel.setBlue(int value): void
+        
+        // Picture.getPixel(x: int, y: int): Pixel
+        // Picture.drawString(text: String, xPos: int, yPos: int): void
+        // Picture.getPixels(): Pixel[]
+        // Picture.getWidth(): int
+        // Picture.getHight(): int
+        // Picture.write(String fileName): boolean
+        
+        Picture bug = img("caterpillar");
+        
+        // bug.getPixel(5, 0).setColor(new Color(255, 0, 0));
+        // setAt(bug, 5, 0, new Color(255, 0, 0));
+        
+        Pixel[] pxs = bug.getPixels();
+        
+        // pxs[0].setColor(new Color(1, 2, 3));
+        
+        int i = 0;
+        for(Pixel px : pxs) {
+            if(i % 2 == 0) {
+                px.setRed(ran(px.getRed()));
+                px.setBlue(ran(px.getBlue()));
+                px.setGreen(ran(px.getGreen()));
+            }
+            i++;
+        }
+        
+        bug.explore();
+    }
+    public static double avg(Pixel px) {
+        return px.getAverage();
+    }
+    public static Picture img(String name) {
+        return new Picture(String.format("images/%s.jpg", name));
+    }
+    public static int ran(int max) { return (int)(Math.random() * max); }
+    public static Color ranRGB() { return new Color(ran(255), ran(255), ran(255)); }
+}
